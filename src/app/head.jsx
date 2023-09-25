18d949ca27147bf70932bd2b9984ccb66b6c0bbf
kianafirozian <kianafirozian96@gmail.com>
2023-09-25 20:18:02 +0330

    install next js and create the first template

---
+++ src/app/head.jsx
@@ -0,0 +1,28 @@
+export default function Head() {
+  return (
+    <>
+      <title>Google Clone</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Google clone created by Next js 13" />
+      <link rel="icon" href="/favicon.svg" />
+    </>
+  );
+}
+
+// test for changing title
+// export default function IndexPage() {
+//   return (
+//     <div>
+
+//       <Head>
+//         <title>My page title</title>
+//         <meta property="og:title" content="My page title" key="title" />
+//       </Head>
+//       <Head>
+//         <meta property="og:title" content="My new title" key="title" />
+//       </Head>
+//       <p>Hello world!</p>
+
+//     </div>
+//   );
+// }
---
+++ src/app/page.jsx
@@ -0,0 +1,6 @@
+import Head from "next/head";
+import React from "react";
+
+export default function home() {
+  return <div>hello world</div>;
+}
