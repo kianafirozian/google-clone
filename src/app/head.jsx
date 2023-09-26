7c95fef18bc373013f57f83e0f9bc2263806bca9
kianafirozian <kianafirozian96@gmail.com>
2023-09-26 08:14:13 +0330

    install tailwind CSS

--- src/app/head.jsx
+++ src/app/head.jsx
@@ -8,21 +8,3 @@ export default function Head() {
     </>
   );
 }
-
-// test for changing title
-// export default function IndexPage() {
-//   return (
-//     <div>
-
-//       <Head>
-//         <title>My page title</title>
-//         <meta property="og:title" content="My page title" key="title" />
-//       </Head>
-//       <Head>
-//         <meta property="og:title" content="My new title" key="title" />
-//       </Head>
-//       <p>Hello world!</p>
-
-//     </div>
-//   );
-// }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,5 +2,5 @@ import Head from "next/head";
 import React from "react";
 
 export default function home() {
-  return <div>hello world</div>;
+  return <div className="text-red-800 text-3xl">hello world</div>;
 }
