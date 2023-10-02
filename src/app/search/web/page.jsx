29bcb40fc95a316d69b22370f16fd0ee75750685
kianafirozian <kianafirozian96@gmail.com>
2023-10-03 01:07:18 +0330

    fetch data from google search api and show the titles

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,3 +1,16 @@
-export default function WebSearchPage() {
-  return <div>WebSearchPage</div>;
+// we change the function to ASYNC so that we can fetch our data with await so it becomes server side rendering
+export default async function WebSearchPage({ searchParams }) {
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
+  );
+  const data = await response.json();
+  const results = data.items;
+
+  return (
+    <>
+      {results &&
+        results.map((result) => <h1 key={result.id}>{result.title}</h1>)}
+      ;
+    </>
+  );
 }
