ddbb18b54f58db6af02aac08033f3582b3d63d72
kianafirozian <kianafirozian96@gmail.com>
2023-10-21 00:19:17 +0330

    change loading name

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,7 @@
 import Footer from "@/components/Footer";
 import "./globals.css";
 import { Suspense } from "react";
-import Loading from "./search/web/loadinggg";
+import Loading from "./search/web/loading";
 // children is page.jsx
 
 export default function RootLayout({ children }) {
