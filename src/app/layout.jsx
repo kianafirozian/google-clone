9639b3897ce52331e1d674d7b1ffe0451d3fd871
kianafirozian <kianafirozian96@gmail.com>
2023-10-21 00:23:03 +0330

    remove unused

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,16 +1,11 @@
 import Footer from "@/components/Footer";
 import "./globals.css";
-import { Suspense } from "react";
-import Loading from "./search/web/loading";
-// children is page.jsx
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="relative min-h-screen">
         {children}
-
-        {/* Footer */}
 
         <Footer />
       </body>
